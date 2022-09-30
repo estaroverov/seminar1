ba8404eb0dfa3e58b16e8a78e9fbbf38ae22de5f
eduard <you@example.com>
2022-09-30 20:38:20 +0300

    Создали программу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя: ");
+string name = Console.ReadLine();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(name);
+Console.Write($"Квадрат {number} = {number*number}");
